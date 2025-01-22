8059237dee87401df61ad33e2913e3a27d28f982
Felipe Jara <fjararibet@gmail.com>
2025-01-22 18:22:59 -0300

    move mouth according to audio file

--- Alien/alien.gd
+++ Alien/alien.gd
@@ -1,9 +1,14 @@
 extends Node3D
 
 @onready var animation_player: AnimationPlayer = $"mouth rig/AnimationPlayer"
-
+@onready var audio_stream_player_2d: AudioStreamPlayer2D = $AudioStreamPlayer2D
 const RhubarbParser = preload("res://rhubarb_parser.gd")
 
+var wait_timer: float = 0.5
+var curr_animation: String = "B"
+var mouth_positions = []
+var next_mouth_position = 0
+var time_since_start: float = 0
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	const file_rel_path = "res://flaca.wav"
@@ -13,21 +18,38 @@ func _ready() -> void:
 		"Windows":
 			const rhubarb_relative_path = "res://rhubarb-lip-sync/rhubarb.exe"
 			var rhubarb_path = ProjectSettings.globalize_path(rhubarb_relative_path)
-			var windows_rhubarb_path = rhubarb_path.replace("/", "\\")
-			
-
+			var windows_rhubarb_path = rhubarb_path.replace("/", "\\")	
 			var windows_file_path = file_path.replace("/", "\\")
 			var arguments = ["/C", windows_rhubarb_path, "-r", "phonetic", "--extendedShapes", "A", windows_file_path]
 			var output = []
 			var exit_code = OS.execute("cmd.exe", arguments, output)
-			var result = output[0]
-			print(rhubarb_parser.parse(result))
+			mouth_positions = rhubarb_parser.parse(output[0])
 		"Linux":
 			var arguments = ["-r", "phonetic", "--extendedShapes", "", file_path]
 			var output = []
 			var exit_code = OS.execute("rhubarb", arguments, output)
-			print(rhubarb_parser.parse(output[0]))
-			
+			mouth_positions = rhubarb_parser.parse(output[0])
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if abs(wait_timer) < 0.01:
+		audio_stream_player_2d.play()
+		time_since_start = 0
+	if not audio_stream_player_2d.playing and wait_timer < 0:
+		animation_player.play("A")
+		wait_timer = 0.5
+		next_mouth_position = 0
+	
+	if audio_stream_player_2d.playing and next_mouth_position < mouth_positions.size():
+		var timestamp = mouth_positions[next_mouth_position][0]
+		var mouth_position = mouth_positions[next_mouth_position][1]
+		
+		if time_since_start > timestamp:
+			animation_player.play(mouth_position, 0.2)
+			next_mouth_position += 1
+		time_since_start += delta
+	
+	wait_timer -= delta
+
+		
+	
