e0f9836414a94ed193ff080b2041b37333d21ab8
Felipe Jara <fjararibet@gmail.com>
2025-01-22 17:39:05 -0300

    run rhubarb in linux

--- Alien/alien.gd
+++ Alien/alien.gd
@@ -6,19 +6,28 @@ const RhubarbParser = preload("res://rhubarb_parser.gd")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	const rhubarb_relative_path = "res://rhubarb-lip-sync/rhubarb.exe"
-	var rhubarb_path = ProjectSettings.globalize_path(rhubarb_relative_path)
-	var windows_rhubarb_path = rhubarb_path.replace("/", "\\")
-	
 	const file_rel_path = "res://flaca.wav"
 	var file_path = ProjectSettings.globalize_path(file_rel_path)
-	var windows_file_path = file_path.replace("/", "\\")
-	var arguments = ["/C", windows_rhubarb_path, "-r", "phonetic", "--extendedShapes", "A", windows_file_path]
-	var output = []
-	var exit_code = OS.execute("cmd.exe", arguments, output)
 	var rhubarb_parser = RhubarbParser.new()
-	var result = output[0]
-	print(rhubarb_parser.parse(result))
+	match OS.get_name():
+		"Windows":
+			const rhubarb_relative_path = "res://rhubarb-lip-sync/rhubarb.exe"
+			var rhubarb_path = ProjectSettings.globalize_path(rhubarb_relative_path)
+			var windows_rhubarb_path = rhubarb_path.replace("/", "\\")
+			
+
+			var windows_file_path = file_path.replace("/", "\\")
+			var arguments = ["/C", windows_rhubarb_path, "-r", "phonetic", "--extendedShapes", "A", windows_file_path]
+			var output = []
+			var exit_code = OS.execute("cmd.exe", arguments, output)
+			var result = output[0]
+			print(rhubarb_parser.parse(result))
+		"Linux":
+			var arguments = ["-r", "phonetic", "--extendedShapes", "", file_path]
+			var output = []
+			var exit_code = OS.execute("rhubarb", arguments, output)
+			print(rhubarb_parser.parse(output[0]))
+			
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
--- rhubarb_parser.gd
+++ rhubarb_parser.gd
@@ -1,7 +1,12 @@
 extends Object
 
 func parse(input: String) -> Array:
-	var split_input = input.split("\r\n")
+	var split_input: PackedStringArray = []
+	match OS.get_name():
+		"Windows":
+			split_input = input.split("\r\n")
+		"Linux":
+			split_input = input.split("\n")
 	var result = []
 	for pair in split_input:
 		var split_pair = pair.split("\t")
