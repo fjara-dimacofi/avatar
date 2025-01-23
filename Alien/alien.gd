59703bc802715d8e5c9df9dc368495f924f9092c
Felipe Jara <fjararibet@gmail.com>
2025-01-23 17:30:05 -0300

    play voice response

--- Alien/alien.gd
+++ Alien/alien.gd
@@ -2,17 +2,47 @@ extends Node3D
 
 @onready var animation_player: AnimationPlayer = $"mouth rig/AnimationPlayer"
 @onready var audio_stream_player: AudioStreamPlayer = $AudioStreamVoice
+@onready var voice_recorder: Node = $VoiceRecorder
 const RhubarbParser = preload("res://rhubarb_parser.gd")
+const GdScriptAudioImport = preload("res://GDScriptAudioImport.gd")
 
-var wait_timer: float = 0.5
+var wait_timer: float = -1
 var curr_animation: String = "B"
 var mouth_positions = []
 var next_mouth_position = 0
 var time_since_start: float = 0
+var pending_dialog: bool = false
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	const file_rel_path = "res://flaca.wav"
-	var file_path = ProjectSettings.globalize_path(file_rel_path)
+	voice_recorder.voice_response_ready.connect(_generate_rhubarb_lipsync)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if abs(wait_timer) < 0.01 and pending_dialog:
+		audio_stream_player.play()
+		time_since_start = 0
+		pending_dialog = false
+	if not audio_stream_player.playing and wait_timer < 0:
+		animation_player.play("A")
+		wait_timer = 0.5
+		next_mouth_position = 0
+	
+	if audio_stream_player.playing and next_mouth_position < mouth_positions.size():
+		var timestamp = mouth_positions[next_mouth_position][0]
+		var mouth_position = mouth_positions[next_mouth_position][1]
+		
+		if time_since_start > timestamp:
+			animation_player.play(mouth_position, 0.2)
+			next_mouth_position += 1
+		time_since_start += delta
+	
+	wait_timer -= delta
+	
+func _generate_rhubarb_lipsync():
+	var audio_import = GdScriptAudioImport.new()
+	var new_stream = audio_import.loadfile(voice_recorder.response_path)
+	audio_stream_player.stream = new_stream
+	var file_path = ProjectSettings.globalize_path(voice_recorder.recording_path)
 	var rhubarb_parser = RhubarbParser.new()
 	match OS.get_name():
 		"Windows":
@@ -29,29 +59,4 @@ func _ready() -> void:
 			var output = []
 			var exit_code = OS.execute("rhubarb", arguments, output)
 			mouth_positions = rhubarb_parser.parse(output[0])
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	if true:
-		return
-	if abs(wait_timer) < 0.01:
-		audio_stream_player.play()
-		time_since_start = 0
-	if not audio_stream_player.playing and wait_timer < 0:
-		animation_player.play("A")
-		wait_timer = 0.5
-		next_mouth_position = 0
-	
-	if audio_stream_player.playing and next_mouth_position < mouth_positions.size():
-		var timestamp = mouth_positions[next_mouth_position][0]
-		var mouth_position = mouth_positions[next_mouth_position][1]
-		
-		if time_since_start > timestamp:
-			animation_player.play(mouth_position, 0.2)
-			next_mouth_position += 1
-		time_since_start += delta
-	
-	wait_timer -= delta
-
-		
-	
+	pending_dialog = true
--- voice_recorder.gd
+++ voice_recorder.gd
@@ -4,7 +4,9 @@ var effect
 var recording
 var recording_name: String = "user_voice.wav"
 var recording_path = "user://" + recording_name
+var response_path = "user://llm_voice.wav"
 var _thread = Thread.new()
+signal voice_response_ready
 
 func _ready() -> void:
 	var idx = AudioServer.get_bus_index("Record")
@@ -44,6 +46,10 @@ func _text_to_llm(text):
 	
 func _llm_to_voice(text):
 	var output = []
-	var command = "echo " + text + " | .venv/bin/piper --model voices/es_MX-claude-high.onnx --output_file llm_voice.wav"
+	var command = "echo " + text + \
+	 " | .venv/bin/piper --model voices/es_MX-claude-high.onnx --output_file " \
+	+ ProjectSettings.globalize_path(response_path)
 	OS.execute("sh", ["-c", command], output, true)
-	print("done", output)
+	print("recording ready")
+	call_deferred("emit_signal", "voice_response_ready")
+	
